f86fbe6b4d9a181a4753b226376bf1741e17d4e3
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-02-03 03:34:52 +0000

    working version

--- js/main.js
+++ js/main.js
@@ -1,42 +1,88 @@
-/*const d = new Date();
-let hour = d.getHours();
-document.getElementById("h").innerHTML = hour;
-let minutes = d.getMinutes();
-document.getElementById("m").innerHTML = minutes;
-let seconds = d.getSeconds();
-document.getElementById("s").innerHTML = seconds;
+var alarmString = null;
 
-let clock = hour + ":" + minutes + ":" + seconds;
-document.getElementById("clock").innerHTML = time;*/
+// Select HTML5 Audio element
+const alarmAudio = document.getElementById("alarm-audio");
+
+// Select DOM element with create-alarm id
+const createAlarm = document.querySelector(".create-alarm");
+
+// Select DOM element of active alarm container
+const activeAlarm = document.getElementById("active-alarm");
+const clearAlarm = document.getElementById("clear-alarm");
+
+// Select DOM element of active alarm text
+const alarmTextContainer = document.getElementById("alarm-text");
+
+const alarmText = (time) => `Alarm set at time ${time}`;
+
+// Initialize alarm window
+const alarmAlert = document.getElementById("alarm-alert")
 
 
-/*function addZero(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
+// Handle Create Alarm submit
+const handleSubmit = (event) => {
+  // Prevent default action of reloading the page
+  event.preventDefault();
+  const { hour, sec, min, zone } = document.forms[0];
+  alarmString = getTimeString({
+    hours: hour.value,
+    seconds: sec.value,
+    minutes: min.value,
+    zone: zone.value
+  });
+  // Reset form after submit
+  document.forms[0].reset();
+  // Hide create alarm
+  createAlarm.style.display = "none";
+  // show active alarm with text
+  activeAlarm.style.display = "block";
+  alarmTextContainer.innerHTML = alarmText(alarmString);
+};
+
+const handleClear = () => {
+  alarmString = "";
+  activeAlarm.style.display = "none";
+  createAlarm.style.display = "block";
+};
+
+// Trigger handleClear on button click
+clearAlarm.addEventListener("click", handleClear);
+// Attach submit event to the form
+document.forms[0].addEventListener("submit", handleSubmit);
+
+// Function to check if alarm needs to be triggered
+const checkAlarm = (timeString) => {
+  if (alarmString === timeString) {
+    alert("It's Time!!");
   }
-  
-  const d = new Date();
-  let h = addZero(d.getHours());
-  let m = addZero(d.getMinutes());
-  let s = addZero(d.getSeconds());
-  let time = h + ":" + m + ":" + s;
-  document.getElementById("clock").innerHTML = time;*/
+};
 
-let setAlarm = document.getElementById
-let alarmTime = document.getElementById
-let myAlarm = 
-setInterval(myClock, 1000);
+// Function to convert time to string value
+const getTimeString = ({ hours, minutes, seconds, zone }) => {
+  if (minutes / 10 < 1) {
+    minutes = "0" + minutes;
+  }
+  if (seconds / 10 < 1) {
+    seconds = "0" + seconds;
+  }
+  return `${hours}:${minutes}:${seconds} ${zone}`;
+};
 
-function myClock() {
-    const d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-}
-  
-if (myTimer == myAlarm){
-    alert('Alarm is going off')
-}
+// Function to display current time on screen
+const renderTime = () => {
+  var currentTime = document.getElementById("current-time");
+  const currentDate = new Date();
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  var zone = hours >= 12 ? "PM" : "AM";
+  if (hours > 12) {
+    hours = hours % 12;
+  }
+  const timeString = getTimeString({ hours, minutes, seconds, zone });
+  checkAlarm(timeString);
+  currentTime.innerHTML = timeString;
+};
 
-
-let hour = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-
-
+// Update time every second
+setInterval(renderTime, 1000);
